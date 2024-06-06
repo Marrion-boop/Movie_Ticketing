b16f20c571e61a201ff38c5e761b8a9504e4e20b
MENTALBOOMING101 <muffinman7134@gmail.com>
2024-06-06 22:51:52 +0800

    asd
    asd

---
+++ ex12_salvador.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <?php 
+        $a=10; $b=20; $c=30;
+        if($a<$b):
+        echo"\$a is less than \$b";
+        endif;
+        echo"<br>";
+        for($i=1;$i<11;$i++):
+            echo $i." ";
+        endfor;
+        ?>
+    </body>
+</html>
